88c9d8ab905ea7ce75058f7dbc022eaa318aa40a
Korrok <skreeler@gmail.com>
2020-07-05 14:40:05 +0200

    initial upload

---
+++ Second Hunt/Assets/Scripts/LevelChange.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class LevelChange: MonoBehaviour {
+ 
+void Update(){
+if(Input.GetKeyDown(KeyCode.Alpha1)){
+Application.LoadLevel("Sanctorus");
+}
+if(Input.GetKeyDown(KeyCode.Alpha2)){
+Application.LoadLevel("arcterra");
+}
+}
+}
---
+++ Second Hunt/README.md
@@ -0,0 +1,3 @@
+# Metroid Prime's HUD Visual | Mix and Jam
+
+This is the official respository for the fifteenth episode of the [Mix and Jam Youtube Channel](https://www.youtube.com/channel/UCLyVUwlB_Hahir_VsKkGPIA)!
