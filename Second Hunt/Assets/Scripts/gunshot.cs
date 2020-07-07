e236bef854c4d547378488268474d5651e7bd93c
Korrok <skreeler@gmail.com>
2020-07-07 18:44:57 +0200

    SFX added
    SFX for powerbeam

---
+++ Second Hunt/Assets/Scripts/gunshot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunshot : MonoBehaviour
+{
+    
+    private AudioSource source;
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    
+    void Update() 
+    {
+        // When spacebar is hit
+         if (Input.GetMouseButtonDown(0))  
+        {
+            // instantiate the fireball object
+            
+            // play the sound
+            source.Play();
+        }
+    }
+
+}
---
+++ Second Hunt/Assets/Scripts/gunshot_loading.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunshot_loading : MonoBehaviour
+{
+    float counter = 0;
+    private AudioSource source;
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    
+    void Update() 
+    {
+		if (Input.GetButton("Fire1")) {
+		counter += Time.deltaTime;
+}
+        // When spacebar is hit
+         if (counter == 1)  
+        {
+            // instantiate the fireball object
+            
+            // play the sound
+            source.Play();
+			counter = 0;
+        }
+    }
+
+}
